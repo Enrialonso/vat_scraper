f0769796e0f976b3e507688f98527ae31f159b11
proclamer <enrialonso@gmail.com>
2021-11-09 14:23:10 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,40 @@
+from playwright.sync_api import Playwright, sync_playwright
+
+
+def crawler(playwright: Playwright) -> None:
+    browser = playwright.chromium.launch(headless=True)
+    context = browser.new_context()
+    page = context.new_page()
+    with open('txt.txt', "r") as file:
+        for line in file.readlines():
+            line = line.replace('\n', '')
+            page.goto("https://www.vatsearch.co.za/vat-search")
+            page.click("[placeholder=\"10 Digit VAT Number\"]")
+            page.fill("[placeholder=\"10 Digit VAT Number\"]", line)
+            page.click("text=Perform Search")
+            page.wait_for_load_state()
+            with open("output.csv", "a") as csv:
+                try:
+                    table_ok = page.query_selector("//*[@id='scrollToSearchResults']")
+                    rows = table_ok.query_selector_all("//tbody")
+                    for row in rows:
+                        td = row.query_selector_all("td")
+                        print(td[0].inner_text())
+                        csv.write(f"{line};{td[0].inner_text()};\n")
+                except Exception as error:
+                    print("NO RESPONSE")
+                    line = line.replace('\n', '')
+                    csv.write(f"{line};NOT-FOUND;\n")
+
+    context.close()
+    browser.close()
+
+
+def main():
+    print("START CRAWLER")
+    with sync_playwright() as playwright:
+        crawler(playwright)
+
+
+if __name__ == "__main__":
+    main()
