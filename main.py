72b90aa4f9329acd5168451a5f99780d48fd29be
proclamer <enrialonso@gmail.com>
2021-11-09 18:21:37 +0100

    improve

--- main.py
+++ main.py
@@ -8,11 +8,19 @@ def crawler(playwright: Playwright) -> None:
     with open('txt.txt', "r") as file:
         for line in file.readlines():
             line = line.replace('\n', '')
-            page.goto("https://www.vatsearch.co.za/vat-search")
-            page.click("[placeholder=\"10 Digit VAT Number\"]")
-            page.fill("[placeholder=\"10 Digit VAT Number\"]", line)
-            page.click("text=Perform Search")
-            page.wait_for_load_state()
+            flag = True
+            while flag:
+                try:
+                    page.goto("https://www.vatsearch.co.za/vat-search")
+                    page.wait_for_load_state("load")
+                    page.click("[placeholder=\"10 Digit VAT Number\"]")
+                    page.fill("[placeholder=\"10 Digit VAT Number\"]", line)
+                    page.click("text=Perform Search", timeout=1000)
+                    flag = False
+                except Exception as error:
+                    print(f"Reload web!!!: {error}")
+
+            page.wait_for_load_state("load")
             with open("output.csv", "a") as csv:
                 try:
                     table_ok = page.query_selector("//*[@id='scrollToSearchResults']")
